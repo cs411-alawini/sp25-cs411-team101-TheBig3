0656c8e4ee7c012148ec280b05776427d0f2bf7a
owen-wang-student <owang420@gmail.com>
2025-03-29 23:34:27 -0500

    crated query for getting top vacation spots for a city

--- db/queries.sql
+++ db/queries.sql
@@ -1,7 +1,7 @@
 -- Our 4 advanced queries 
 
 -- 1. User Following Feed Retrieval 
--- Get reviews within the last 30 days from users the current user follows and the top 10 users with the most followers. 
+-- Get reviews within the last 14 days from users the current user follows and the top 10 users with the most followers. 
 
 SELECT ReviewID, Username, ReviewText, ReviewRating, CreatedAt, LikeCount
 FROM (
@@ -28,8 +28,18 @@ ORDER BY CreatedAt DESC;
 -- 2. Vacation Spot Review Page 
 -- When defaultly viewing vacation spot reviews, finds 3 most relevant reviews for a given vacation spot (including images) based on popularity (liked or most recent) 
 
--- 3. Top 5 City Vacation Spots - can be used for heat map
--- When defaultly viewing city, finds 5 most relevant vacation spots (including images) based its popularity (ranked by number of reviews, average rating, how many recent reviews)
+-- 3. Top City Vacation Spots 
+-- When viewing city, find the most relevant vacation spots (including images) based on its popularity determined by number of reviews and average rating
+
+SELECT vs.VacationSpotName, c.CityName, COUNT(r.ReviewID) AS TotalReviews, AVG(r.ReviewRating) AS AverageRating, i.ImageURL
+FROM VacationSpots vs
+    JOIN Cities c ON vs.CityId = c.CityId
+    JOIN VacationSpotReviews vr ON vs.VacationSpotName = vr.VacationSpotName
+    JOIN Reviews r ON vr.ReviewId = r.ReviewID
+    LEFT JOIN Images i ON r.ReviewID = i.ReviewID
+WHERE c.CityName = 'CityName'
+GROUP BY vs.VacationSpotName, c.CityName, i.ImageURL
+ORDER BY TotalReviews DESC, AverageRating DESC
 
 -- 4. User Favorite Spot Retrieval?
 -- Gets users favorited spots based ?
